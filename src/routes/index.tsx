f2fcc126b8b505ad3a0e1520dc022d09fe45147d
Jason <bilafish@gmail.com>
2023-06-16 17:59:07 +0800

    chore: update head

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
@@ -15,11 +15,12 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Pomodoro App",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content:
+        "Pomdoro is a simple and effective app that helps you boost your productivity and focus. Pomdoro uses the Pomodoro Technique, a time management method that breaks down your work into 25-minute intervals of focused work followed by 5-minute breaks. You can customize your work and break durations, track your progress, and get notified when it’s time to switch modes. Pomdoro is the perfect app for anyone who wants to get more done in less time and enjoy a balanced work-life rhythm.",
     },
   ],
 };
