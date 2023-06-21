757675fc1b1961855ed53b431ea61c86b99bf0e9
Jason Z <bilafish@gmail.com>
2023-06-21 11:23:50 +0800

    tweak: add timer state logic

--- src/components/circular-progress.tsx
+++ src/components/circular-progress.tsx
@@ -1,16 +1,39 @@
+import type { Signal } from "@builder.io/qwik";
 import { component$, useStyles$ } from "@builder.io/qwik";
 import styles from "./circular-progress.css?inline";
 
-export const CircularProgress = component$(() => {
-  useStyles$(styles);
-  return (
-    <div class="percent">
-      <svg>
-        <circle cx="150" cy="150" r="150"></circle>
-        <circle cx="150" cy="150" r="150"></circle>
-      </svg>
-      <h1>17:59</h1>
-      <h3>pause</h3>
-    </div>
-  );
-});
+interface CircularProgressProps {
+  isRunning: Signal<boolean>;
+  dateTimeStarted: Signal<Date | null>;
+  timeLeft: string;
+  timeElapsed: Signal<number>;
+}
+
+export const CircularProgress = component$(
+  ({ isRunning, dateTimeStarted, timeLeft, timeElapsed }: CircularProgressProps) => {
+    useStyles$(styles);
+    return (
+      <div class="percent">
+        <svg>
+          <circle cx="150" cy="150" r="150"></circle>
+          <circle cx="150" cy="150" r="150"></circle>
+        </svg>
+        <h1>{timeLeft}</h1>
+        <button
+          onClick$={() => {
+            if (!isRunning.value) {
+              dateTimeStarted.value = new Date();
+            } else {
+              const now = new Date();
+              timeElapsed.value = now.getTime() - dateTimeStarted.value.getTime();
+              dateTimeStarted.value = null;
+            }
+            isRunning.value = !isRunning.value;
+          }}
+        >
+          <h3>{isRunning.value ? "pause" : "start"}</h3>
+        </button>
+      </div>
+    );
+  }
+);
--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,10 +1,49 @@
-import { component$, useStyles$ } from "@builder.io/qwik";
+import {
+  component$,
+  useStyles$,
+  useSignal,
+  useVisibleTask$,
+} from "@builder.io/qwik";
 import styles from "./timer.css?inline";
 import { CircularProgress } from "./circular-progress";
 
+function msToMMSS(ms: number) {
+  const min = Math.floor((ms / 1000 / 60) % 60);
+  const sec = Math.floor((ms / 1000) % 60);
+  const minString = min < 10 ? "0" + min : min;
+  const secString = sec < 10 ? "0" + sec : sec;
+  return minString + ":" + secString;
+}
+
 export const Timer = component$(() => {
+  const timerDuration = 10000;
+  const isRunning = useSignal(false);
+  const dateTimeStarted = useSignal(null);
+  const timeElapsed = useSignal(0);
+  const timeLeft = useSignal("");
   useStyles$(styles);
-  return <div class="container">
-    <CircularProgress/>
-  </div>;
+  useVisibleTask$(({ track, cleanup }) => {
+    track(() => isRunning.value);
+    let id: NodeJS.Timer | undefined;
+    if (dateTimeStarted.value === null) return;
+    if (isRunning.value) {
+      id = setInterval(() => {
+        const now = new Date();
+        const duration = now.getTime() - dateTimeStarted.value!.getTime();
+        const remaining = timerDuration - duration - timeElapsed.value;
+        timeLeft.value = remaining > 0 ? msToMMSS(remaining) : msToMMSS(0);
+      }, 500);
+    }
+    cleanup(() => clearInterval(id));
+  });
+  return (
+    <div class="container">
+      <CircularProgress
+        isRunning={isRunning}
+        dateTimeStarted={dateTimeStarted}
+        timeLeft={timeLeft.value}
+        timeElapsed={timeElapsed}
+      />
+    </div>
+  );
 });
